b735a36e9dff4a7863840bbfb46a3ec47b176737
Michael Canales <CDLSMichael.16@gmail.com>
2023-06-16 21:07:40 -0500

    finished

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import WeatherDetail from "../../components/WeatherDetail";
 
@@ -10,7 +9,6 @@ export const Home = () => {
       <div className="min-h-screen flex justify-center items-center p-2">
       <div 
         className="w-full max-w-600px flex min-h-550px rounded-md justify-center items-center mt-10">
-        <Link to="/about">About</Link>
           <WeatherDetail />
       </div>
       </div>
