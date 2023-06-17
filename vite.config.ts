81e12d70ed055c6033e6b4f84209afce5efd847f
Michael Canales <CDLSMichael.16@gmail.com>
2023-06-16 20:01:38 -0500

    data

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 3000
-  }
-  
-})
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      path: "path-browserify",
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 2500,
+  },
+});
