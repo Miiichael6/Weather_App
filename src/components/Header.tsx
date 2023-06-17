515966ef8194868696ae1d0e726ef2cdc56c4380
Michael Canales <CDLSMichael.16@gmail.com>
2023-06-16 19:36:35 -0500

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import Form from "./Form";
+import weather from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header className="w-full bg-cyan-700 py-3 px-10 flex items-center justify-between fixed top-0 sm:flex-row md: flex-col">
-      <p className="text-2xl text-white font-thin mb-1 inline-block">
-        WeatherApp
-      </p>
+      <div className="flex items-center gap-2">
+        <div className="w-12">
+          <img src={weather} alt="weather-logo" />
+        </div>
+        <p className="text-2xl text-teal-50 font-thin mb-1 inline-block">
+          WeatherApp
+        </p>
+      </div>
+      <div className="py-1"></div>
       <Form />
     </header>
   );
